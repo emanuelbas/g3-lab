e9d600293d5a771837e61d8675574265fd7a2927
emanuelbas <emanuelbastons@gmail.com>
2021-09-16 13:12:05 -0300

    arreglo puerto

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', require('./routes/index'))
 
-app.listen(3000);
-console.log('Server on port', 3000);
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`)
+})
+
+//app.listen(3000);
+//console.log('Server on port', 3000);
 
 

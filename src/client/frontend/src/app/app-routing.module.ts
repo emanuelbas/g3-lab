8c10156273445051d89fd31932d07fec623ed8d8
emanuelbas <emanuelbastons@gmail.com>
2021-09-19 13:55:07 -0300

    Agrego componentes de prueba ng

--- src/client/frontend/src/app/app-routing.module.ts
+++ src/client/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { TareasComponent } from './components/tareas/tareas.component';
+import { TareasPrivadasComponent } from './components/tareas-privadas/tareas-privadas.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tareas',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tareas',
+    component: TareasComponent
+  },
+  {
+    path: 'tareas-privadas',
+    component: TareasPrivadasComponent
+  },
+  {
+    path: 'ingresar',
+    component: IngresarComponent
+  },
+  {
+    path: 'registrar',
+    component: RegistrarComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/frontend/src/app/app.module.ts
+++ src/client/frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { TareasPrivadasComponent } from './components/tareas-privadas/tareas-privadas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarComponent,
+    IngresarComponent,
+    TareasComponent,
+    TareasPrivadasComponent
   ],
   imports: [
     BrowserModule,

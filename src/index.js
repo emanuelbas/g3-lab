3edfb4e876440f8dfb0cfd44eef40a56d705a538
emanuelbas <emanuelbastons@gmail.com>
2021-09-19 02:27:08 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 require('./database');
 
@@ -9,6 +10,13 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', require('./routes/index'))
 
+// Serve static files
+app.use(express.static(__dirname + '/client/frontend/dist/frontend'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/client/frontend/dist/frontend/index.html'));
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {

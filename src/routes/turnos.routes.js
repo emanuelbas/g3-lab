ac5f56071e794172e8d23f3bae33e043ae488f05
emanuelbas <emanuelbastons@gmail.com>
2021-12-12 04:53:35 -0300

    Turnos - Logica para obtener turnos ocupados

--- src/routes/turnos.routes.js
+++ src/routes/turnos.routes.js
@@ -3,8 +3,8 @@ const router = Router();
 const turnoControllers = require('../controllers/turno.controllers');
 
 router.get('/prueba-turnos', turnoControllers.pruebaTurnos);
-
-
+router.get('/get-turnos/:date', turnoControllers.pruebaTurnos);
+router.post('/tomar-turno/:datetime', turnoControllers.pruebaTurnos);
 
 
 module.exports = router
